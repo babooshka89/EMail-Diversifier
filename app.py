1245e9f8cc436025a29313f0addd9e35e415925e
lurr98 <laurazeidler.konstanz@gmail.com>
2025-09-25 11:19:53 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+
+import streamlit as st
+
+st.session_state.page = "main_page"
+
+st.markdown("""
+    # Herzlich Wilkommen!
+            
+    Du willst eine personalisierte E-Mail schreiben? Dann bist du bei unserem Tool hier richtig! 
+    """
+)
