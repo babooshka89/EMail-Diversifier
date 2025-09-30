b6a06cf278c21b60de57767d38ac7b3ae8ccce8e
lurr98 <laurazeidler.konstanz@gmail.com>
2025-09-30 12:55:42 +0200

    changed format

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.markdown("""
     Das hier ist der originale Text:\n\n
     """)
 
-st.markdown("``` " + template + " ```")
+st.markdown("```\n" + template + "\n```")
 
 if st.button("Neue Version generieren"):
     
