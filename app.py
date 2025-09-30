a4520f583b61225c8e447ba388dd9097bfe03076
lurr98 <laurazeidler.konstanz@gmail.com>
2025-09-30 11:25:44 +0200

    added original text on website

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ from scripts.generate_div_version import generate_definition_gemini
 
 st.session_state.page = "main_page"
 
+with open("vorlage.txt", "r") as vl:
+    template = vl.read()
 st.markdown("""
     # Herzlich Wilkommen!
             
-    Du willst eine personalisierte E-Mail schreiben? Dann bist du bei unserem Tool hier richtig! 
+    Du willst eine personalisierte E-Mail schreiben? Dann bist du bei unserem Tool hier richtig!\n\n
+        Das hier ist der originale Text:\n\n    
     """
 )
+st.markdown(template)
 if st.button("Neue Version generieren"):
-    with open("vorlage.txt", "r") as vl:
-        template = vl.read()
+    
     new_template = generate_definition_gemini(template)
     st.write(new_template)
