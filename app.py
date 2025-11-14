4246d458b4f9efbbfde556f1359695e6981b45de
lurr98 <laurazeidler.konstanz@gmail.com>
2025-11-14 13:21:06 +0100

    added columns

--- app.py
+++ app.py
@@ -18,13 +18,13 @@ st.markdown("""
     Du willst eine personalisierte E-Mail schreiben? Dann bist du bei unserem Tool hier richtig!\n\n
     """)
 
-col1, col2 = st.columns(2)
+col = st.columns([0.5, 0.5],gap="small")
 
-with col1:
+with col[0]:
     if st.button("Neue Version generieren"):
         subject, new_template = generate_definition_gemini("\n".join(template))
         st.session_state.text = "changed"
-with col2:
+with col[1]:
     if st.button("Originalen Text anzeigen"):
         st.session_state.text = "original"
         new_template = original_str
